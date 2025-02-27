b00ae7726c94af3a86c948c53f92fa65aaad6b2f
daniel <139523036+danny-codes@users.noreply.github.com>
2025-02-26 20:07:53 -0800

    constructor for making book objects

--- organizing-js-code/objects-objects-constructors/script.js
+++ organizing-js-code/objects-objects-constructors/script.js
@@ -7,6 +7,19 @@ function Player(name, marker) {
 }
 
 const player1 = new Player('steve', 'X');
-const player2 = new Player('also steve', 'O')
+const player2 = new Player('also steve', 'O');
 player1.sayName();
 player2.sayName();
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        return `${title} by ${author}, ${pages} pages, ${read ? 'not read yet' : 'read'}`;
+    }
+}
+
+const theHobbit = new Book('The hobbit', 'J.R.R. Tolkien', '295', false);
+console.log(theHobbit.info());
