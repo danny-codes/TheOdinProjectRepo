dfb9abb25eabae55bde4a20ac95a18ab58c90ef3
daniel <139523036+danny-codes@users.noreply.github.com>
2025-02-28 21:56:06 -0800

    add basic constructor, array

---
+++ library-project/script.js
@@ -0,0 +1,20 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        return `${title} by ${author}, ${pages} pages, ${read ? 'read' : 'not read yet'}`;
+    }
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+}
+
+for (i in myLibrary) {
+    // create cards for each book
+}
