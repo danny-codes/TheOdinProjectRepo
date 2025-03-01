c67deb8ebf375e7b3ead98e311c133047c040c13
daniel <139523036+danny-codes@users.noreply.github.com>
2025-02-28 19:01:46 -0800

    creating constructors and understanding prototype inheritance

--- organizing-js-code/objects-objects-constructors/script.js
+++ organizing-js-code/objects-objects-constructors/script.js
@@ -23,3 +23,39 @@ function Book(title, author, pages, read) {
 
 const theHobbit = new Book('The hobbit', 'J.R.R. Tolkien', '295', false);
 console.log(theHobbit.info());
+
+function Hero(name, level) {
+    this.name = name;
+    this.level = level;
+}
+
+// let hero1 = new Hero('A', 1);
+// Object.getPrototypeOf(hero1);
+
+Hero.prototype.greet = function () {
+    return `${this.name} says hello.`;
+}
+
+function Warrior(name, level, weapon) {
+    Hero.call(this, name, level);
+    this.weapon = weapon;
+}
+
+function Healer(name, level, spell) {
+    Hero.call(this, name, level);
+    this.spell = spell;
+}
+
+Warrior.prototype.attack = function () {
+    return `${this.name} attacks with the ${this.weapon}`;
+}
+
+Healer.prototype.heal = function () {
+    return `${this.name} casts ${this.spell}.`;
+}
+
+const hero1 = new Warrior('Bjorn', 1, 'axe');
+const hero2 = new Healer('Kanin', 1, 'cure');
+
+Object.setPrototypeOf(Warrior.prototype, Hero.prototype);
+Object.setPrototypeOf(Healer.prototype, Warrior.prototype);
