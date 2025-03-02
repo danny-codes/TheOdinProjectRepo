e2e66910f65b8a1700c3c97e57b9790044da0f9f
daniel <139523036+danny-codes@users.noreply.github.com>
2025-03-02 13:46:54 -0800

    function to display books, event listener to make form appear and disappear as needed, function to add input values to an array

--- library-project/script.js
+++ library-project/script.js
@@ -1,5 +1,11 @@
 const myLibrary = [];
 
+const body = document.body;
+const container = document.querySelector('.container');
+const newBookBtn = document.querySelector('#new-book');
+const bookForm = document.querySelector('#book-form');
+const addBookBtn = document.querySelector('#add-book');
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -13,8 +19,77 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(title, author, pages, read) {
     let newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
+    // displayBooks(newBook);
+    displayBooks();
 }
 
-for (i in myLibrary) {
-    // create cards for each book
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, true);
+
+function displayBooks(book) {
+    container.innerHTML = "";
+
+    myLibrary.forEach((book) => {
+        let card = document.createElement("div");
+        card.classList.add("book-card");
+        container.appendChild(card);
+        // card.setAttribute('data-index', myLibrary[i])
+
+        let header = document.createElement("h3");
+        header.textContent = book.title;
+        card.appendChild(header);
+
+        let authorP = document.createElement("p");
+        authorP.textContent = `Author: ${book.author}`;
+        card.appendChild(authorP);
+
+        let pagesP = document.createElement("p");
+        pagesP.textContent = `${book.pages} pages`;
+        card.appendChild(pagesP);
+
+        let readStatus = document.createElement("p");
+        readStatus.textContent = book.read ? "Read" : "Not read yet";
+        card.appendChild(readStatus);
+    });
 }
+
+newBookBtn.addEventListener('click', () => {
+    bookForm.style.display = 'block';
+});
+
+function addInputToArray() {
+    const titleInput = document.getElementById('title');
+    const authorInput = document.getElementById('author');
+    const pagesInput = document.getElementById('pages');
+    const isReadInput = document.getElementById('read');
+
+    const titleValue = titleInput.value;
+    const authorValue = authorInput.value;
+    const pagesValue = parseInt(pagesInput.value);
+    const isReadValue = isReadInput.checked;
+    console.log(isReadValue);
+
+    if (titleValue && authorValue &&
+        pagesValue && isReadValue) {
+            myLibrary.push({title: titleValue, author: authorValue,
+                pages: pagesValue, read: isReadValue
+            });
+        titleInput.value = '';
+        authorInput.value = '';
+        pagesValue.value = '';
+        isReadInput.checked = false;
+        console.table(myLibrary);
+        }
+    else {
+        alert('Please fill in all fields.');
+    }
+}
+
+addBookBtn.addEventListener('click', (e) => {
+    bookForm.style.display = 'none';
+    e.preventDefault();
+});
+
+// for (let i = 0; i < myLibrary.length; i++) {
+//     console.log(myLibrary[i]);
+//     console.log('for loop working');
+// }
