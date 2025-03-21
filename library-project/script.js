e046c7f497edf49da5dea89b623cba6f9f340524
daniel <139523036+danny-codes@users.noreply.github.com>
2025-03-21 12:09:34 -0700

    add projects for gh-pages

---
+++ library-project/script.js
@@ -0,0 +1,107 @@
+const myLibrary = [];
+
+const body = document.body;
+const container = document.querySelector('.container');
+const newBookBtn = document.querySelector('#new-book');
+const bookForm = document.querySelector('#book-form');
+const addBookBtn = document.querySelector('#add-book');
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+Book.prototype.info = function() {
+    return `${title} by ${author}, ${pages} pages, ${read ? 'read' : 'not read yet'}`;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    displayBooks();
+}
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, true);
+
+function displayBooks(book) {
+    container.innerHTML = "";
+
+    myLibrary.forEach((book, index) => {
+        let card = document.createElement("div");
+        card.classList.add("book-card");
+        container.appendChild(card);
+
+        card.setAttribute('data-index', index)
+
+        let header = document.createElement("h3");
+        header.textContent = book.title;
+        card.appendChild(header);
+
+        let authorP = document.createElement("p");
+        authorP.textContent = `Author: ${book.author}`;
+        card.appendChild(authorP);
+
+        let pagesP = document.createElement("p");
+        pagesP.textContent = `${book.pages} pages`;
+        card.appendChild(pagesP);
+
+        // let readStatus = document.createElement("p");
+        let readStatus = document.createElement("button");
+        readStatus.textContent = book.read ? "Read" : "Not read yet";
+        card.appendChild(readStatus);
+        
+        let removeBook = document.createElement('button');
+        removeBook.classList.add('remove-btn');
+        removeBook.textContent = 'Remove book';
+        card.appendChild(removeBook);
+        
+        let cardIndex = card.getAttribute('data-index');
+
+        readStatus.addEventListener('click', () => {
+            book.read ? book.read = false : book.read = true;
+            readStatus.textContent = book.read ? "Read" : "Not read yet";
+        });
+        
+        removeBook.addEventListener('click', () => {
+            myLibrary.splice(index, 1);
+            card.remove();
+        });
+    });
+}
+
+newBookBtn.addEventListener('click', () => {
+    bookForm.style.display = 'block';
+});
+
+function addInputToArray() {
+    const titleInput = document.getElementById('title');
+    const authorInput = document.getElementById('author');
+    const pagesInput = document.getElementById('pages');
+    const isReadInput = document.getElementById('read');
+
+    const titleValue = titleInput.value;
+    const authorValue = authorInput.value;
+    const pagesValue = parseInt(pagesInput.value);
+    const isReadValue = isReadInput.checked;
+    console.log(isReadValue);
+
+    if (titleValue && authorValue && pagesValue) {
+        addBookToLibrary(titleValue, authorValue, pagesValue, isReadValue);
+
+        titleInput.value = '';
+        authorInput.value = '';
+        pagesInput.value = '';
+        isReadInput.checked = false;
+        console.table(myLibrary);
+    }
+    else {
+        alert('Please fill in all fields.');
+    }
+}
+
+addBookBtn.addEventListener('click', (e) => {
+    bookForm.style.display = 'none';
+    e.preventDefault();
+});
---
+++ tic-tac-toe/script.js
@@ -0,0 +1,156 @@
+const Gameboard = (function () {
+    let gameboard = ['', '', '', '', '', '', '', '', ''];
+    const renderBoard = () => {
+        let grid = '';
+        for (let i = 0; i < 3; i++) {
+            grid += ` ${gameboard.slice(i * 3, i * 3 + 3).join(' |  ')} \n`;
+            if (i < 2) grid += '---+---+---\n'
+        }
+        console.log(grid);
+    }      
+    return {
+        renderBoard: renderBoard,
+        gameboard: gameboard,
+    }
+})();
+
+let cells = document.querySelectorAll('.cell');
+const startBtn = document.querySelector('#startBtn');
+const restartBtn = document.querySelector('#restartBtn');
+const results = document.querySelector('#results');
+let playerXInput = document.querySelector('#playerX');
+let playerOInput = document.querySelector('#playerO');
+let playerXName = playerXInput.value;
+let playerOName = playerOInput.value;
+let showDiv = false;
+let xImg = document.querySelectorAll('.xImg');
+let oImg = document.querySelectorAll('.oImg');
+
+let winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+]
+
+const Player = (name, symbol) => ({
+    name,
+    symbol,
+    score: 0,
+    turn: false,
+    isWinner: false,
+    moves: [],
+});
+
+let playerX = Player('Jane', 'X');
+
+let playerO = Player('John', 'O');;
+
+startGame();
+
+function startGame() {
+    playerX.turn = !playerX.turn;
+    playerO.turn = !playerO.turn;
+    let currentPlayer = playerX.turn ? 'X' : 'O';
+
+    cells.forEach(cell => {
+        cell.replaceWith(cell.cloneNode(true));
+    });
+
+    cells = document.querySelectorAll('.cell');
+
+    cells.forEach(cell => {
+        cell.addEventListener('click', (e) => {
+            const index = cell.getAttribute('data-index');
+
+            if (Gameboard.gameboard[index] === '') {
+                Gameboard.gameboard[index] = currentPlayer;
+
+                e.target.classList.add(currentPlayer === 'X' ? 'xImg' : 'oImg');       
+
+                const winner = checkWinner();
+                endGame(winner);
+
+                if (!Gameboard.gameboard.includes('')) {
+                    restartGame();
+                    results.classList.add('showResults');
+                    results.textContent = 'No winner';
+                }
+
+                currentPlayer = (currentPlayer === 'X') ? 'O' : 'X';
+            }
+        });
+    })
+}
+
+startBtn.addEventListener('click', () => {
+    startGame();
+    results.classList.remove('showResults');
+    results.textContent = '';
+    hideImages();
+    playerXInput.value = '';
+    playerOInput.value = '';
+});
+
+function checkWinner() {
+    const board = Gameboard.gameboard;
+    for (const combo of winningCombinations) {
+        const [a, b, c] = combo;
+
+        if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+            return board[a];
+        }
+    }
+    return null;
+}
+
+function hideImages() {
+    cells.forEach(cell => cell.classList.remove('xImg', 'oImg'));
+}
+
+function endGame(winner) {
+    if (winner) {
+
+        playerXName = playerXInput.value;
+        playerOName = playerOInput.value;
+
+        let winnerName = winner === "X" ? playerXName : playerOName;
+
+            if (winnerName !== '') {
+                console.log(`The winner is ${winnerName}`);
+                results.classList.add('showResults');
+                results.textContent = `${winnerName} wins! 🙌 🔥 \nGood job!`;
+            }
+            else {
+                console.log(`${winner} wins!`);
+                results.classList.add('showResults');
+                results.textContent = `${winner} wins! 🙌 \nCongratulations!`;
+            }
+        results.style.whiteSpace = 'pre-line';
+        restartGame();
+    }
+    else {
+        console.log('No winner yet.');
+    }
+}
+
+function restartGame() {
+    Gameboard.gameboard.fill('');
+    cells.forEach(cell => {
+        cell.textContent = '';
+    });
+    console.log('UI Cleared.');
+}
+
+restartBtn.addEventListener('click', () => {
+    restartGame();
+    results.classList.remove('showResults');
+    results.textContent = '';
+    hideImages();
+    playerXName = playerXInput.value;
+    playerOName = playerOInput.value;
+});
