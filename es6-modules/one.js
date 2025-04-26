e546f13963e46cbd313e8b932c72e29600ee8394
daniel <139523036+danny-codes@users.noreply.github.com>
2025-04-26 09:34:55 -0700

    esm exports & imports

---
+++ es6-modules/one.js
@@ -0,0 +1,6 @@
+const greeting = 'Hello!';
+const farewell = 'Bye!';
+
+export default 'Hey!';
+
+export { greeting, farewell };
---
+++ es6-modules/two.js
@@ -0,0 +1,5 @@
+import { greeting, farewell } from "./one.js";
+// import heyString from './one.js'; - default import
+
+console.log(greeting);
+console.log(farewell);
