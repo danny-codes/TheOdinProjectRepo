e57764576fb1a0569b732aaa41e50953faf6ea7f
daniel <139523036+danny-codes@users.noreply.github.com>
2025-05-14 15:54:22 -0700

    create contact load func

---
+++ restaurant-page/src/contact.js
@@ -0,0 +1,16 @@
+export function contactLoad() {
+    const div = document.querySelector('#content');
+
+    div.innerHTML = `
+    <h2 style="text-align:center;">Contact Us</h2>
+    <p>Have questions, feedback, or feature requests?<br>
+    We’d love to hear from you.</p>
+
+    <p>📍 127.0.0.1 Dev Street, Stack City<br>
+    📧 <a href="mailto:contact@domcafe.dev">contact@domcafe.dev</a><br>
+    📞 <a href="tel:1234567890">(123) 456-7890</a></p>
+
+    <p>Open daily from <strong>08:00</strong> to <strong>22:00</strong>.<br>
+    Come grab a byte!</p>
+    `;
+}
