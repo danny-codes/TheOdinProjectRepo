f711a992c688661b096f1d90580bead672972acc
daniel <139523036+danny-codes@users.noreply.github.com>
2025-05-14 15:55:04 -0700

    import all load funcs, add event listener for all nav buttons and trigger load func upon clicking

--- restaurant-page/src/index.js
+++ restaurant-page/src/index.js
@@ -1,6 +1,34 @@
 import './styles.css';
 import { load } from './home';
+import { loadMenu } from './menu';
+import { loadAboutUs } from './about-us';
+import { contactLoad } from './contact';
 document.addEventListener('DOMContentLoaded', () => {
     load();
 })
+
+const div = document.querySelector('#content');
+
+const homeBtn = document.querySelector('#homeBtn');
+const menuBtn = document.querySelector('#menuBtn');
+const aboutBtn = document.querySelector('#aboutBtn');
+const contactBtn = document.querySelector('#contactBtn');
+
+homeBtn.addEventListener('click', () => {
+    div.replaceChildren();
+    load();
+})
+menuBtn.addEventListener('click', () => {
+    div.replaceChildren();
+    loadMenu();
+})
+aboutBtn.addEventListener('click', () => {
+    div.replaceChildren();
+    loadAboutUs();
+})
+contactBtn.addEventListener('click', () => {
+    div.replaceChildren();
+    contactLoad();
+})
+
 console.log('test');
