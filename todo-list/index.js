581ffbeba2bf1cc30d487e91a5aacf8fba47da7c
daniel <139523036+danny-codes@users.noreply.github.com>
2025-05-31 15:01:40 -0700

    for each Project instance, create div, and insert the Project name into p inside div append all of it to #content. add e listeners for projects that clear #content when clicked

--- todo-list/index.js
+++ todo-list/index.js
@@ -1,4 +1,52 @@
-import { Project, defaultProject, createNewProject, renderProject, createNewTodo, Todo } from "./app.js";
+import { Project, defaultProject, createNewProject, createNewTodo, Todo } from "./app.js";
 
 // DOM stuff
 
+console.log(defaultProject.todos);
+
+document.addEventListener('DOMContentLoaded', () => {
+    renderProjects();
+    addProjectListeners();
+});
+
+function renderProjects() {
+    // for each Project instance, create div,
+    // & insert the Project name into p inside div
+    // append all of it to #content
+
+    let content = document.querySelector('#content');
+
+    let header = document.createElement('h1');
+    header.textContent = 'Your projects';
+    header.classList.add('.projectHeader');
+
+    content.appendChild(header);
+
+    Project.instances.forEach(instance => {
+        let div = document.createElement('div');
+        div.classList.add('projectDiv');
+
+        let nameP = document.createElement('p');
+        let projectText = instance.name;
+        nameP.textContent = projectText;
+        nameP.classList.add("projectName");
+
+        content.appendChild(div);
+        div.appendChild(nameP);
+    });
+    // 
+    //
+}
+
+function addProjectListeners() {
+    document.querySelectorAll(".projectDiv").forEach((project) => {
+    project.addEventListener("click", clearContent);
+    });
+}
+
+function clearContent() {
+    let content = document.querySelector('#content');
+    content.replaceChildren();
+}
+
+function renderTodos() {}
