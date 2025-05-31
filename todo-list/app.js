311a182dc32bed61dfda7135824f017911b36bd7
daniel <139523036+danny-codes@users.noreply.github.com>
2025-05-30 21:27:03 -0700

    added basic application classes and function 'templates'

---
+++ todo-list/app.js
@@ -0,0 +1,32 @@
+export { Project, defaultProject, createNewProject, renderProject, createNewTodo, Todo };
+
+class Project {
+    constructor(type) {
+        this.type = type;
+    }
+}
+
+let defaultProject = new Project('default');
+
+function createNewProject() {
+
+};
+
+function renderProject() {
+
+};
+
+function createNewTodo() {
+
+};
+
+class Todo extends Project{
+    constructor(title, description, dueDate, priority, difficulty, notes){
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.difficulty = difficulty;
+        this.notes = notes;
+    }
+}
