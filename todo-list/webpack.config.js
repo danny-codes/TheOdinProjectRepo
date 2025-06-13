e0140dfc748b36dbd52d2e0104a6ba1ad73f9cee
daniel <139523036+danny-codes@users.noreply.github.com>
2025-06-13 14:05:29 -0700

    configure for html, css, fonts

---
+++ todo-list/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    },
+    mode: "development",
+    module: {
+    rules: [
+        {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+        },
+        {
+        test: /\.(woff|woff2|ttf|otf|eot)$/,
+        type: 'asset/resource',
+        generator: {
+        filename: 'fonts/[name][ext]'
+        }
+    }
+    ],
+    plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+    }),
+    ],
+    },
+};
