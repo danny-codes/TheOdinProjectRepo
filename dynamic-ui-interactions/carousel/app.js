81be006de736257a2189280575444ce6c036cb83
daniel <139523036+danny-codes@users.noreply.github.com>
2025-06-22 15:21:14 -0700

    add a timeout to advance slides every 5 seconds

--- dynamic-ui-interactions/carousel/app.js
+++ dynamic-ui-interactions/carousel/app.js
@@ -25,6 +25,11 @@ function showSlides(n) {
     for (i = 0; i < dots.length; i++)  {
         dots[i].className = dots[i].className.replace(' active', '');
     }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
     slides[slideIndex-1].style.display = 'block';
     dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 5000);
 }
