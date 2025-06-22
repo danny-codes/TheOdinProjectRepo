2a6e0356a809a95b75b21f8589eea6fcc1504203
daniel <139523036+danny-codes@users.noreply.github.com>
2025-06-22 15:30:56 -0700

    fix issues with timeout for slideshow

--- dynamic-ui-interactions/carousel/app.js
+++ dynamic-ui-interactions/carousel/app.js
@@ -1,35 +1,40 @@
 let slideIndex = 1;
+let slideTimer;
+
 showSlides(slideIndex);
 
 function plusSlides(n) {
     showSlides(slideIndex += n);
+    resetTimer();
 }
 
 function currentSlide(n) {
     showSlides(slideIndex = n);
+    resetTimer();
 }
 
 function showSlides(n) {
-    let i;
     let slides = document.querySelectorAll('.slide');
     let dots = document.querySelectorAll('.dot');
+
     if (n > slides.length) {
         slideIndex = 1;
     }
     if (n < 1) {
         slideIndex = slides.length;
     }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = 'none';
-    }
-    for (i = 0; i < dots.length; i++)  {
-        dots[i].className = dots[i].className.replace(' active', '');
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    }
+
+    slides.forEach(slide => slide.style.display = 'none');
+    dots.forEach(dot => dot.className = dot.className.replace(' active', ''));
+
     slides[slideIndex-1].style.display = 'block';
     dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 5000);
+    
+    slideTimer = setTimeout(() => {
+        showSlides(slideIndex += 1);
+    }, 5000);
+}
+
+function resetTimer() {
+    clearTimeout(slideTimer);
 }
