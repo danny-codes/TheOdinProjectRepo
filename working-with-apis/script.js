ee46ab18d5b20143bd2f8c73ebd8f843ec4fad07
daniel <139523036+danny-codes@users.noreply.github.com>
2025-07-03 13:00:58 -0700

    feat: implement Giphy API search and image display logic

--- working-with-apis/script.js
+++ working-with-apis/script.js
@@ -1,12 +1,49 @@
-const img = document.querySelector('img');
+const img = document.getElementById('gif');
+const btn = document.querySelector('button');
+const searchInput = document.querySelector('input');
+const thumbnail = document.getElementById('header-thumb');
+
+btn.addEventListener('click', function(e) {
+  e.preventDefault();
+  standardImage();
+});
+
+searchInput.addEventListener('blur', searchImage);
+
+function searchImage() {
+  let input = searchInput.value;
+  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=IlKHuNJYVtEwiiK5eCkdsTaMWmzGzO4o&s=${input}`, {mode: 'cors'})
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(response) {
+    if (Array.isArray(response.data) && response.data.length === 0) {
+      alert('Didn’t find any results for search');
+      standardImage();
+      return;
+    } else {
+    img.src = response.data.images.original.url;
+    thumbnail.src = response.data.images.original.url;
+    }
+  })
+  .catch(e => {
+    console.log(e);
+  })
+};
+
+function standardImage() {
 fetch('https://api.giphy.com/v1/gifs/translate?api_key=IlKHuNJYVtEwiiK5eCkdsTaMWmzGzO4o&s=cats', {mode: 'cors'})
   .then(function(response) {
     return response.json();
   })
   .then(function(response) {
-    // console.log(response.data.images.original.url);
     img.src = response.data.images.original.url;
+    thumbnail.src = response.data.images.original.url;
+
   })
   .catch(e => {
     console.log(e);
   })
+}
+
+standardImage();
