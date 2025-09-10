4f600e09e03e11ab25f54034aac841c16c2df96b
daniel <139523036+danny-codes@users.noreply.github.com>
2025-09-09 18:10:53 -0700

    finishing commit

--- linked-lists/script.js
+++ linked-lists/script.js
@@ -15,10 +15,94 @@ class LinkedList {
         }
         current.nextNode = newNode;
     }
-    // prepend = function (value) {
-    //     let newNode = new Node(value);
 
-    // }
+    prepend = function(value) {
+        let newNode = new Node(value);
+        newNode.next = this.head;
+        this.head = newNode;
+    }
+    
+    size = function() {
+        let current = this.head;
+        let count = 0;
+        while (current.nextNode !== null) {
+            count++;
+            current = current.nextNode;
+        }
+        console.log('Number of nodes: ' + count);
+        return count;
+    }
+
+    headNode = function() {
+        console.log(this.head);
+        return this.head;
+    }
+
+    tail = function() {
+        if (!this.head) return null;
+        let current = this.head;
+
+        while (current.nextNode !== null) {
+            current = current.nextCurrent;
+        }
+        console.log(current);
+        return current;
+    }
+
+    at = function(index) {
+        let current = this.head;
+        for (let i = 0; i < index; i++) {
+            current = current.nextNode;
+        }
+        return current;
+    }
+
+    pop = function() {
+        if (!this.head) return;
+        if (!this.head.nextNode) {
+            this.head = null;
+            return;
+        }
+        let current = this.head;
+        while (current.nextNode.nextNode !== null) {
+            current = current.nextNode;
+        }
+        current.nextNode = null;
+    }
+
+    contains = function(value) {
+        let current = this.head;
+        // if (current === value) return true;
+        while (current !== null) {
+            if (current.value === value) return true;
+            current = current.nextNode;
+        }
+        return false;
+    }
+
+    find = function(value) {
+        let current = this.head;
+        let index = 0;
+        while (current !== null) {
+            if (current.value === value) return index;
+            current = current.nextNode;
+            index++;
+        }
+        return null;
+        
+    }
+
+    toString = function() {
+        let str = '';
+        let current = this.head;
+        while (current !== null) {
+            str += `( ${current.value} -> )`;
+            current = current.nextNode;
+        }
+        str += 'null';
+        return str;
+    }
+    
 }
 
 class Node {
@@ -27,3 +111,17 @@ class Node {
         this.nextNode = nextNode;
     }
 }
+
+const list = new LinkedList();
+
+list.prepend("animal");
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+
+list.size();
+list.headNode();
+console.log(list.toString());
