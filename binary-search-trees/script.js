3d887d085b129f47db8cf61cdf9340c96052bb7b
daniel <139523036+danny-codes@users.noreply.github.com>
2026-06-07 21:04:00 -0700

    implemented buildTree function

---
+++ binary-search-trees/script.js
@@ -0,0 +1,35 @@
+export {Node, Tree};
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+    // insert, search for, delete
+    
+}
+
+class Tree {
+    constructor(array) {
+        this.array = array;
+        this.root = this.buildTree(array);
+    }
+
+    buildTree(array) {
+        if (array.length === 0) return null;
+        // takes array -> balanced binary tree 
+        const midIndex = Math.floor(array.length / 2)
+        const midValue = array[midIndex];
+        const root = new Node(midValue);
+        const leftSide = array.slice(0, midIndex);
+        const rightSide = array.slice(midIndex + 1);
+    
+        root.left = this.buildTree(leftSide);
+        root.right = this.buildTree(rightSide);
+
+        return root; 
+    }
+
+    includes(value) {}
+}
